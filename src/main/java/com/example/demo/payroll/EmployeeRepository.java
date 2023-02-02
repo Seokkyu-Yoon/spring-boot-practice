0cc8932a5dfb08834bb7f9883c36e2fc1aad5e55
윤석규 <yskyu93@modutech.co.kr>
2023-02-02 13:45:14 +0900

    feat(payroll): add employees RestController with inmemory database

--- src/main/java/com/example/demo/payroll/EmployeeRepository.java
+++ src/main/java/com/example/demo/payroll/EmployeeRepository.java
@@ -2,6 +2,4 @@ package com.example.demo.payroll;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface EmployeeRepository extends JpaRepository<Employee, Long> {
-  
-}
+interface EmployeeRepository extends JpaRepository<Employee, Long> {}
